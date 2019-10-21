0af435003bb81d5eef7464ce6f3a7f3828f3f8fa
Matthew <vansoem1@tcnj.edu>
2019-10-20 23:24:46 -0400

    Added crop image function
    crop image function added,
    dimensions: (topLeft),(bottomRight)

--- ImageWork.py
+++ ImageWork.py
@@ -41,7 +41,42 @@ def get_image_array(image):
             temp.append(image.getpixel((i,j)))
     return pixelMatrix
 
+def crop_image(dimentions,image):
+    '''
+    Recieves an image and dimensions,
+    outputs a new image cropped to new dimenstions
+    (arg1) dimentions : string entered as "(x1,y1),(x2,y2)" == (topLeft),(bottomRight)
+    (arg2) image : image to be cropped (PIL image)
+    '''
+    # splices the dimenstions string to extract coordinates as integers
+    x1 = int(dimentions[1:dimentions.find(",")])
+    y1 = int(dimentions[dimentions.find(",")+1:dimentions.find(")")])
+    dimentions = dimentions[dimentions.find(")")+3:]
+    x2 = int(dimentions[:dimentions.find(",")])
+    y2 = int(dimentions[dimentions.find(",")+1:dimentions.find(")")])
+    
+    # x and y conditions check to make coordinates entered are with in bounds
+    # order condition checks if (x1,y1) is less than (y1,y2)
+    width, height = image.size
+    print(image.size)
+    x_condition = x1 > 0 and x1 < width and x2 > 0 and x2 < width
+    y_condition = y1 > 0 and y1 < width and y2 > 0 and y2 < width
+    order_condition = x1 < x2 and y1 < y2
+
+    #if conditions are true returns new cropped image
+    #otherwise returns issue with dimentions string
+    if x_condition and y_condition and order_condition:
+        new_image = image.crop((x1,y1,x2,y2))
+        new_image.show()
+        return new_image
+    elif not order_condition:
+        print("The dimentions entered are out of order")
+    else:
+        print("The dimentions entered are not with-in the bounds of the image")
+
+
 def test():
     '''
     Used for local testing.
     '''
+   
